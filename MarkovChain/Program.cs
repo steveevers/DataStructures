928d5a152626d4a2fd8dbafb793593a382151524
steveevers <steven.evers@outlook.com>
2015-08-05 22:40:00 -0700

    Fairly non-compelling example of a markov chain. Need to get some better data to build the graph of normalized probabilities.
    Want to use a wikipedia dump, but don't have hdd space for that at the moment.

---
+++ MarkovChain/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graphs;
+using Utilities;
+
+namespace MarkovChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var states = new List<char> { 'A', 'B', 'C', 'D' };
+            var m = new MarkovChain<char>(states);
+            
+            for (int i = 0; i < states.Count; i++)
+            {
+                for (int j = 0; j < states.Count; j++)
+                {
+                    float probability = (float)Math.Abs(i - j) * .25f;
+
+                    m.AddTransition(i, j, probability);
+                }
+            }
+
+            int k = 0;
+            List<char> sequence = new List<char>();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("History:\t" + string.Join(",", sequence));
+                Console.WriteLine("Suggestions:\t" + string.Join(",", m.MarkovPath(k, 4)));
+
+                if (!int.TryParse(Console.ReadKey().KeyChar.ToString(), out k))
+                {
+                    continue;
+                }
+
+                if (k == 9 || k >= m.States.Count)
+                    break;
+
+                sequence.Add(m.States[k]);
+            } while (true);
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Done");
+            while (!Console.KeyAvailable) { }
+        }
+    }
+}
